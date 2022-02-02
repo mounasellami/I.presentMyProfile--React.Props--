37619efae9e887b294121c485b5f5bf8a0eaeb86
mounasellami <sellamimouna25@gmail.com>
2022-02-02 15:33:14 +0100

    checkpoint->react.props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Profile from './profile/Profile';
+import ProfilePhoto from './res/ProfilePhoto.jpg';
+
 
 function App() {
   let fullName= "Radhia Mouna Sellami";
   let bio="I learned Automation and Industrial Computing & a Fullstack JS. I will be able to develop both front-end and back-end web applications. I have a dual skills in programing / electric / electronic / automation and I realy love programming";
   let profession="Senior Technician in Automation and Industrial Computing & I’ll be a super web developer";
-  const handleName=(fullName)=>alert({fullName})
+  const handleName=()=>alert(`Hello and welcome to know more details about ${fullName}`)
   return (
     <div className="App">
-      <Profile fullName={fullName} bio={bio} profession={profession} handleName={handleName}/>
+      <Profile fullName={fullName} bio={bio} profession={profession} handleName={handleName}>
+        <img style={{marginTop:"3%", width:"26%", borderRadius:"50%"}} src={ProfilePhoto} alt='My Profile'/>
+      </Profile>
     </div>
   );
 }
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ProfilePhoto from '../res/ProfilePhoto.jpg';
+// import React from 'react';
 import propTypes from 'prop-types';
 
 const position = {
-    float:"right",
     display: "flex",
     justifyContent: "center",
     flexWrap: "wrap"
@@ -12,33 +10,29 @@ const customStyle = {
     fontFamily: "Sacramento, cursive",
     fontSize: "5rem",
     color: "pink",
-    textAlign: "center"
+    textAlign: "center",
+    margin: "1%"
 }
 const bioAndprofession = {
-    color: "black",
+    color: "white",
     lineHeight: "1.7",
     fontSize: "1.3rem",
     fontWeight: "normal",
     textAlign: "center"
 }
-const positionimg = {
-    margin:"3%",
-    width:"50%",
-    borderRadius: "50%",
-    justifyContent: "spacebettween",
-}  
 
-const Profile = ({fullName, bio, profession}) => {
+
+
+const Profile = ({fullName, bio, profession,handleName,children}) => {
   return (
     <div style={position}>
-       <div>      
-         <img style={positionimg} src={ProfilePhoto} alt='My Profile Photo'/>
-       </div>
-       <div>
-         <h1 style={customStyle}>{fullName}</h1>
-         <h2 style={bioAndprofession}>{bio}</h2>
-         <p style={bioAndprofession}>{profession}</p>
-       </div>
+      {handleName(fullName)}
+      {children}     
+      <div>
+        <h1 style={customStyle}>{fullName}</h1>
+        <h2 style={bioAndprofession}>{bio}</h2>
+        <p style={bioAndprofession}>{profession}</p>
+      </div>
     </div>
   )
 }
